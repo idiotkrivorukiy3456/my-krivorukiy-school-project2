f5c95640048bf080e370e1d07d85796007a2aaaa
idiotkrivorukiy3456 <pisuuvokno777hour@gmail.com>
2025-10-07 19:19:49 +0700

    Add files via upload

---
+++ Новая папка (2)/Script1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+<script>// Функция для скрытия/показа содержимого разделов
+    function toggleContent(contentId) {
+          const content = document.getElementById(contentId);
+    content.classList.toggle('show');
+
+          // Обновляем текст заголовка
+
+          }</script>
